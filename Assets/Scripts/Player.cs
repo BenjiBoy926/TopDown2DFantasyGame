feaf13e5cb4674b8f1d23a8972fdea996f9d85c0
Codey Huntting <147837412+PrincepsDraconum@users.noreply.github.com>
2026-02-27 21:38:06 -0600

    repacking some methods

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,42 +38,43 @@ public class Player : MonoBehaviour, DefaultActions.IPlayerActions
 
     public void OnCancel(InputAction.CallbackContext context)
     {
-        // Return unit back to original position
+        CancelMove();
     }
 
     public void OnCursorPosition(InputAction.CallbackContext context)
     {
         Vector2 screenPosition = context.ReadValue<Vector2>();
         Vector2 newPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        Vector2 oldPosition = transform.position;
-
-        // TODO: change to setter so we can also easily set position using WASD
-        transform.position = newPosition;
-
-        Vector2 positionOnGrid = _grid.Round(newPosition);
-        _gridPosition.position = positionOnGrid;
-
-        if (_character)
-        {
-            _character.Position = newPosition;
-            _character.SetDirection(newPosition - oldPosition);
-        }
+        SetPosition(newPosition);
     }
 
     public void OnCursorPress(InputAction.CallbackContext context)
     {
         if (context.started && !_character)
         {
-            CaptureNewCharacter();
+            StartMove();
         }
-        // Change to making character either attack or defend
         if (context.canceled && _character)
         {
-            SetCharacter(null);
+            FinishMove();
         }
     }
 
-    private void CaptureNewCharacter()
+    private void SetPosition(Vector2 newPosition)
+    {
+        if (_character)
+        {
+            Vector2 oldPosition = transform.position;
+            _character.Position = newPosition;
+            _character.SetDirection(newPosition - oldPosition);
+        }
+        transform.position = newPosition;
+
+        Vector2 positionOnGrid = _grid.Round(newPosition);
+        _gridPosition.position = positionOnGrid;
+    }
+
+    private void StartMove()
     {
         Character characterAtCursor = GetCharacterAtCursor();
         if (characterAtCursor)
@@ -82,25 +83,39 @@ public class Player : MonoBehaviour, DefaultActions.IPlayerActions
         }
     }
 
-    private Character GetCharacterAtCursor()
-    {
-        Vector2 gridPosition = _gridPosition.position;
-        int count = Physics2D.OverlapBoxNonAlloc(gridPosition, Vector2.one, 0, _overlapResults);
-        return count > 0 ? _overlapResults[0].GetComponentInParent<Character>() : null;
-    }
-
-    public void SetCharacter(Character character)
+    private void FinishMove()
     {
         if (_character)
         {
             // TODO: change to coroutine that runs in character to smooth move
-            _character.Position = _grid.Round(_character.Position);
+            _character.Position = _gridPosition.position;
             _character.SetIsRunning(false);
+            SetCharacter(null);
         }
+    }
+
+    private void CancelMove()
+    {
+        if (_character)
+        {
+            // TODO: move character back to starting position
+            // SetCharacter(null);
+        }
+    }
+
+    private void SetCharacter(Character character)
+    {
         _character = character;
         if (_character)
         {
             _character.SetIsRunning(true);
         }
+    }
+
+    private Character GetCharacterAtCursor()
+    {
+        Vector2 gridPosition = _gridPosition.position;
+        int count = Physics2D.OverlapBoxNonAlloc(gridPosition, Vector2.one, 0, _overlapResults);
+        return count > 0 ? _overlapResults[0].GetComponentInParent<Character>() : null;
     }
 }
