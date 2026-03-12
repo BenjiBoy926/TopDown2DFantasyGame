fa6479f8f4f456c0d6a4bf950217d3a74eddac81
Codey Huntting <147837412+PrincepsDraconum@users.noreply.github.com>
2026-03-12 11:45:05 -0500

    moved turn logic into its own script

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -1,47 +1,27 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Battlefield))]
+[RequireComponent(typeof(BattleTurn))]
 public class Battle : MonoBehaviour
 {
-    private Faction CurrentFaction => _factions[_currentFactionIndex];
-
     private Battlefield _field;
-    private readonly HashSet<Character> _characters = new();
-    private readonly List<Faction> _factions = new(2);
-    private int _currentFactionIndex = 0;
-    private readonly List<Character> _characterListScratch = new();
+    private BattleTurn _turn;
 
     public void Register(Character obj)
     {
-        _characters.Add(obj);
+        _turn.Register(obj);
         _field.Register(obj);
-
-        Faction faction = obj.Faction;
-        if (!_factions.Contains(faction))
-        {
-            _factions.Add(faction);
-        }
     }
 
     public void Unregister(Character obj)
     {
-        _characters.Remove(obj);
+        _turn.Unregister(obj);
         _field.Unregister(obj);
-        GetCharactersInFaction(obj.Faction, _characterListScratch);
-        if (_characterListScratch.Count == 0)
-        {
-            _factions.Remove(obj.Faction);
-        }
     }
 
     public void StartNextTurn()
     {
-        _currentFactionIndex = (_currentFactionIndex + 1) % _factions.Count;
-        foreach (Character character in _characters)
-        {
-            character.RestoreMove();
-        }
+        _turn.StartNextTurn();
     }
 
     public Vector3 SnapToGrid(Vector3 position)
@@ -67,60 +47,6 @@ public class Battle : MonoBehaviour
     private void Awake()
     {
         _field = GetComponent<Battlefield>();
-    }
-
-    private void OnEnable()
-    {
-        Character.UsedMove += OnCharacterUsedMove;
-    }
-
-    private void OnDisable()
-    {
-        Character.UsedMove -= OnCharacterUsedMove;
-    }
-
-    private void OnCharacterUsedMove(Character obj)
-    {
-        if (CountCharactersThatCanStillMove(CurrentFaction) == 0)
-        {
-            StartNextTurn();
-        }
-    }
-
-    private void AddCharacterFaction(Character obj)
-    {
-        Faction faction = obj.Faction;
-        if (!_factions.Contains(faction))
-        {
-            _factions.Add(faction);
-        }
-    }
-
-    private int CountCharactersThatCanStillMove(Faction faction)
-    {
-        GetCharactersInFaction(CurrentFaction, _characterListScratch);
-
-        int canStillMove = 0;
-        for (int i = 0; i < _characterListScratch.Count; i++)
-        {
-            Character character = _characterListScratch[i];
-            if (!character.HasMovedThisTurn)
-            {
-                canStillMove++;
-            }
-        }
-        return canStillMove;
-    }
-
-    private void GetCharactersInFaction(Faction faction, List<Character> characters)
-    {
-        characters.Clear();
-        foreach (Character character in _characters)
-        {
-            if (character.Faction == faction)
-            {
-                characters.Add(character);
-            }
-        }
+        _turn = GetComponent<BattleTurn>();
     }
 }
---
+++ Assets/Scripts/BattleTurn.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTurn : MonoBehaviour
+{
+    private Faction CurrentFaction => _factions[_currentFactionIndex];
+
+    private readonly HashSet<Character> _characters = new();
+    private readonly List<Faction> _factions = new(2);
+    private int _currentFactionIndex = 0;
+    private readonly List<Character> _characterListScratch = new();
+
+    public void Register(Character obj)
+    {
+        _characters.Add(obj);
+
+        Faction faction = obj.Faction;
+        if (!_factions.Contains(faction))
+        {
+            _factions.Add(faction);
+        }
+    }
+
+    public void Unregister(Character obj)
+    {
+        _characters.Remove(obj);
+        GetCharactersInFaction(obj.Faction, _characterListScratch);
+        if (_characterListScratch.Count == 0)
+        {
+            _factions.Remove(obj.Faction);
+        }
+    }
+
+    public void StartNextTurn()
+    {
+        _currentFactionIndex = (_currentFactionIndex + 1) % _factions.Count;
+        foreach (Character character in _characters)
+        {
+            character.RestoreMove();
+        }
+    }
+
+    private void OnEnable()
+    {
+        Character.UsedMove += OnCharacterUsedMove;
+    }
+
+    private void OnDisable()
+    {
+        Character.UsedMove -= OnCharacterUsedMove;
+    }
+
+    private void OnCharacterUsedMove(Character obj)
+    {
+        if (CountCharactersThatCanStillMove(CurrentFaction) == 0)
+        {
+            StartNextTurn();
+        }
+    }
+
+    private int CountCharactersThatCanStillMove(Faction faction)
+    {
+        GetCharactersInFaction(CurrentFaction, _characterListScratch);
+
+        int canStillMove = 0;
+        for (int i = 0; i < _characterListScratch.Count; i++)
+        {
+            Character character = _characterListScratch[i];
+            if (!character.HasMovedThisTurn)
+            {
+                canStillMove++;
+            }
+        }
+        return canStillMove;
+    }
+
+    private void GetCharactersInFaction(Faction faction, List<Character> characters)
+    {
+        characters.Clear();
+        foreach (Character character in _characters)
+        {
+            if (character.Faction == faction)
+            {
+                characters.Add(character);
+            }
+        }
+    }
+}
