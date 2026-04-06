e4cb225b9a1d6830ae1419ac2bcdd6f3cd3b5f3b
CodeyHuntting <creativecodey@gmail.com>
2026-04-06 14:59:14 -0500

    confirm that I can read the projection matrix
    I might be able to use this to convert a screen offset to a world offset by scaling it, without offsetting the position

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -7,18 +7,21 @@ public class PlayerCamera : MonoBehaviour
     private Camera _camera;
     private Rigidbody2D _rigidbody;
     private Vector2 _grabScreenPosition;
+    private Vector2 _grabCameraPosition;
 
     public void Grab(Vector2 worldPosition)
     {
         Vector2 screenPosition = _camera.WorldToScreenPoint(worldPosition);
         _grabScreenPosition = screenPosition;
+        _grabCameraPosition = _rigidbody.position;
     }
 
     public void UpdateFromScreenPosition(Vector2 screenPosition)
     {
         Vector2 offset = screenPosition - _grabScreenPosition;
+        Matrix4x4 projection = _camera.projectionMatrix;
         Debug.Log($"Screen offset: {offset}");
-        // do something?
+        // have to convert that screen offset to a world offset using... the projection matrix?  The ortho size?
     }
 
     public Vector2 ScreenToWorld(Vector2 screen)
