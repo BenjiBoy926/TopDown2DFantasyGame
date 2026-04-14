8d0d395236d3ec91b42a350b35ffa46c2b5eff26
CodeyHuntting <creativecodey@gmail.com>
2026-04-14 11:34:21 -0500

    remove debug log

--- Assets/Scripts/Battle/BattleTurn.cs
+++ Assets/Scripts/Battle/BattleTurn.cs
@@ -42,7 +42,6 @@ public class BattleTurn : MonoBehaviour
     public void StartNextTurn()
     {
         StartTurn(_currentFactionIndex + 1);
-        Debug.Log($"Starting turn for {CurrentFaction}");
     }
 
     private void StartTurn(Faction faction)
