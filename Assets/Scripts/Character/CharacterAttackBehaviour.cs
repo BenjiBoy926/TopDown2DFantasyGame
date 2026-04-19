dab03a08e4cf68f291ac869a168b0cd08298d653
CodeyHuntting <creativecodey@gmail.com>
2026-04-19 14:40:23 -0500

    getting better

--- Assets/Scripts/Character/CharacterAttackBehaviour.cs
+++ Assets/Scripts/Character/CharacterAttackBehaviour.cs
@@ -11,52 +11,60 @@ public class CharacterAttackBehaviour : MonoBehaviour
     [Space]
     [SerializeField] private float _chargeDuration = 0.09f;
     [SerializeField] private Ease _chargeEase = Ease.InBack;
+
+    [Space]
     [SerializeField] private float _attackConnectPause = 1;
 
     [Space]
     [SerializeField] private float _fallbackDuration = .2f;
     [SerializeField] private Ease _fallbackEase = Ease.OutBack;
 
-    private static readonly WaitForSeconds _dealDamageWait = new(.1f);
     private Character _character;
 
     public IEnumerator GetSequence(Character other)
     {
         _character.SecureCurrentCell();
         _character.LookAt(other.Position);
+        other.LookAt(_character.Position);
 
+        Vector2 cellPosition = _character.CellToWorld(_character.CurrentCell);
+
+        _character.PlayAttackAnimation();
+        yield return CharacterChargeSequence(other);
+        yield return AttackConnectSequence(other);
+
+        Coroutine otherFlinchRoutine = StartCoroutine(OtherFlinchSequence(other));
+        yield return _character.transform.DOMove(cellPosition, _fallbackDuration)
+            .SetEase(_fallbackEase)
+            .WaitForCompletion();
+        yield return otherFlinchRoutine;
+        yield return _character.MoveFadeOut();
+    }
+
+    private IEnumerator CharacterChargeSequence(Character other)
+    {
         Vector2 cellPosition = _character.CellToWorld(_character.CurrentCell);
         Vector2 towardsTarget = (other.Position - cellPosition).normalized;
         Vector2 chargeOffset = towardsTarget * _character.CellSize * 0.49f;
         Vector2 chargePosition = cellPosition + chargeOffset;
 
-        _character.PlayAttackAnimation();
         yield return _character.transform.DOMove(cellPosition, _moveToCellCenterDuration)
             .SetEase(_moveToCellCenterEase)
             .WaitForCompletion();
         yield return _character.transform.DOMove(chargePosition, _chargeDuration)
             .SetEase(_chargeEase)
             .WaitForCompletion();
+    }
 
-        WaitForSeconds pauseWait = new(_attackConnectPause);
+    private IEnumerator AttackConnectSequence(Character other)
+    {
         _character.PauseAnimation();
-        yield return pauseWait;
+        yield return other.transform.DOShakePosition(_attackConnectPause).WaitForCompletion();
         _character.ResumeAnimation();
-
-        Coroutine otherFlinchRoutine = StartCoroutine(OtherFlinchSequence(other));
-        yield return _character.transform.DOMove(cellPosition, _fallbackDuration)
-            .SetEase(_fallbackEase)
-            .WaitForCompletion();
-
-        yield return otherFlinchRoutine;
-        yield return _character.MoveFadeOut();
     }
 
     private IEnumerator OtherFlinchSequence(Character other)
     {
-        other.LookAt(_character.Position);
-        yield return _dealDamageWait;
-
         other.TakeDamageFrom(_character);
         yield return other.PlayHurtAnimation();
         if (other.IsDead)
