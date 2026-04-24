41b4d0936146c3bf84d6b5f9c66cc29fba9d3281
CodeyHuntting <creativecodey@gmail.com>
2026-04-24 15:44:42 -0500

    the ui sprites fade
    need to figure out how to fade the label

--- Assets/Scripts/Character/UI/CharacterUI.cs
+++ Assets/Scripts/Character/UI/CharacterUI.cs
@@ -1,12 +1,17 @@
+using DG.Tweening;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterUI : MonoBehaviour
 {
     private static readonly HashSet<CharacterUI> _instances = new();
 
+    [SerializeField] private float _hideAlpha = .1f;
     private CharacterHealthUI _healthUI;
     private CharacterPowerUI _powerUI;
+    private SpriteRenderer[] _renderers;
+    private TMP_Text[] _labels;
 
     public static void HideAll()
     {
@@ -41,18 +46,28 @@ public class CharacterUI : MonoBehaviour
 
     public void Hide()
     {
-        gameObject.SetActive(false);
+        foreach (var renderer in _renderers)
+        {
+            renderer.DOKill();
+            renderer.DOFade(_hideAlpha, 0.1f);
+        }
     }
 
     public void Show()
     {
-        gameObject.SetActive(true);
+        foreach (var renderer in _renderers)
+        {
+            renderer.DOKill();
+            renderer.DOFade(1, 0.1f);
+        }
     }
 
     private void Awake()
     {
         _healthUI = GetComponentInChildren<CharacterHealthUI>();
         _powerUI = GetComponentInChildren<CharacterPowerUI>();
+        _renderers = GetComponentsInChildren<SpriteRenderer>();
+        _labels = GetComponentsInChildren<TMP_Text>();
         _instances.Add(this);
     }
 
