2b3c4508047c6b13f60f17f2074d647678b1464d
CodeyHuntting <creativecodey@gmail.com>
2026-05-04 16:00:30 -0500

    added parameters for spiral but doesn't do it yet

--- Assets/Scripts/Character/CharacterHealBehaviour.cs
+++ Assets/Scripts/Character/CharacterHealBehaviour.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(Character))]
 public class CharacterHealBehaviour : MonoBehaviour
 {
+    [SerializeField] private float _moveToSpiralStartDuration = .5f;
+    [SerializeField] private Ease _moveToSpiralStartEase = Ease.OutQuad;
+
+    [Space]
+    [SerializeField] private float _spiralRadius = 0.49f;
+    [SerializeField] private int _spiralTurns = 3;
+    [SerializeField] private float _spiralTurnDuration = 0.15f;
+
     private Character _character;
 
     public IEnumerator GetSequence(Character other)
@@ -30,11 +38,21 @@ public class CharacterHealBehaviour : MonoBehaviour
 
     private IEnumerator DanceSequence()
     {
-        Vector2 targetPosition = _character.CellToWorld(_character.CurrentCell);
+        Vector2 cellCenter = _character.CellToWorld(_character.CurrentCell);
+        Vector2 spiralStart = cellCenter + Vector2.up * _character.CellSize * _spiralRadius;
         _character.SetIsRunning(true);
-        yield return transform.DOMove(targetPosition, .3f)
-            .SetEase(Ease.OutQuad)
+        yield return transform.DOMove(spiralStart, _moveToSpiralStartDuration)
+            .SetEase(_moveToSpiralStartEase)
             .WaitForCompletion();
+        yield return SpiralSequence();
         _character.SetIsRunning(false);
+    }
+
+    private IEnumerator SpiralSequence()
+    {
+        // Doesn't actually spin yet
+        Vector2 cellCenter = _character.CellToWorld(_character.CurrentCell);
+        _character.Position = cellCenter;
+        yield break;
     }
 }
