aac03b4c16e3c2e9ea5ec473f3816dc955f0b786
CodeyHuntting <creativecodey@gmail.com>
2026-05-04 15:47:43 -0500

    slight improvements to heal behaviour

--- Assets/Scripts/Character/CharacterHealBehaviour.cs
+++ Assets/Scripts/Character/CharacterHealBehaviour.cs
@@ -5,33 +5,36 @@ using UnityEngine;
 [RequireComponent(typeof(Character))]
 public class CharacterHealBehaviour : MonoBehaviour
 {
-    [SerializeField] private float _moveToCellCenterDuration = 0.35f;
     private Character _character;
 
     public IEnumerator GetSequence(Character other)
     {
-        // heal them
+        CharacterUI.HideAll();
         _character.SecureCurrentCell();
-
         _character.LookAt(other.Position);
         if (!other.IsDead)
         {
             other.LookAt(_character.Position);
         }
 
+        yield return DanceSequence();
+        yield return other.BeHealed();
+        yield return _character.MoveFadeOut();
+        CharacterUI.ShowAll();
+    }
+
+    private void Awake()
+    {
+        _character = GetComponent<Character>();
+    }
+
+    private IEnumerator DanceSequence()
+    {
         Vector2 targetPosition = _character.CellToWorld(_character.CurrentCell);
         _character.SetIsRunning(true);
         yield return transform.DOMove(targetPosition, .3f)
             .SetEase(Ease.OutQuad)
             .WaitForCompletion();
         _character.SetIsRunning(false);
-
-        yield return other.BeHealed();
-        yield return _character.MoveFadeOut();
-    }
-
-    private void Awake()
-    {
-        _character = GetComponent<Character>();
     }
 }
