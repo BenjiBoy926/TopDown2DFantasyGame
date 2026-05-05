7130c06dd7b58200f21abf6e2698cedd11577d44
CodeyHuntting <creativecodey@gmail.com>
2026-05-05 10:06:45 -0500

    the punch is decent

--- Assets/Scripts/Character/CharacterBeHealedBehaviour.cs
+++ Assets/Scripts/Character/CharacterBeHealedBehaviour.cs
@@ -5,30 +5,20 @@ using DG.Tweening;
 [RequireComponent(typeof(Character))]
 public class CharacterBeHealedBehaviour : MonoBehaviour
 {
-    [SerializeField] private float _initialDelay = 0.3f;
     [SerializeField] private float _hopHeight = 0.3f;
-    [SerializeField] private float _hopDuration = 0.3f;
-    [SerializeField] private Ease _hopUpEase = Ease.OutQuad;
-    [SerializeField] private Ease _fallBackEase = Ease.OutBounce;
-
+    [SerializeField] private float _hopDuration = 0.7f;
+    [SerializeField] private int _vibrato = 10;
+    [SerializeField] private float _elasticity = 0;
     private Character _character;
 
     public IEnumerator GetSequence()
     {
-        WaitForSeconds initialWait = new(_initialDelay);
-        yield return initialWait;
-
         _character.RestoreHealth();
 
         // TODO: slight change to animation if they are dead and being revived instead of just healed
 
-        Vector2 hopPeak = _character.CurrentCellCenter + _hopHeight * _character.CellHeight * Vector2.up;
-        yield return transform.DOMove(hopPeak, _hopDuration)
-            .SetEase(_hopUpEase)
-            .WaitForCompletion();
-        yield return transform.DOMove(_character.CurrentCellCenter, _hopDuration)
-            .SetEase(_fallBackEase)
-            .WaitForCompletion();
+        Vector2 punch = _character.CellHeight * _hopHeight * Vector2.up;
+        yield return transform.DOPunchPosition(punch, _hopDuration, _vibrato, _elasticity).WaitForCompletion();
     }
 
     private void Awake()
