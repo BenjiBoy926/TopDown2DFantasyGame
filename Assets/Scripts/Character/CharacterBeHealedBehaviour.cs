cff3e8931714b2af759f0255bfb43e47eea1fee1
CodeyHuntting <creativecodey@gmail.com>
2026-05-05 10:00:28 -0500

    it technically works
    have to finagle with the numbers to make it look snappier

--- Assets/Scripts/Character/CharacterBeHealedBehaviour.cs
+++ Assets/Scripts/Character/CharacterBeHealedBehaviour.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 [RequireComponent(typeof(Character))]
 public class CharacterBeHealedBehaviour : MonoBehaviour
 {
+    [SerializeField] private float _initialDelay = 0.3f;
+    [SerializeField] private float _hopHeight = 0.3f;
+    [SerializeField] private float _hopDuration = 0.3f;
+    [SerializeField] private Ease _hopUpEase = Ease.OutQuad;
+    [SerializeField] private Ease _fallBackEase = Ease.OutBounce;
+
     private Character _character;
 
     public IEnumerator GetSequence()
     {
+        WaitForSeconds initialWait = new(_initialDelay);
+        yield return initialWait;
+
         _character.RestoreHealth();
-        yield break;
+
+        // TODO: slight change to animation if they are dead and being revived instead of just healed
+
+        Vector2 hopPeak = _character.CurrentCellCenter + _hopHeight * _character.CellHeight * Vector2.up;
+        yield return transform.DOMove(hopPeak, _hopDuration)
+            .SetEase(_hopUpEase)
+            .WaitForCompletion();
+        yield return transform.DOMove(_character.CurrentCellCenter, _hopDuration)
+            .SetEase(_fallBackEase)
+            .WaitForCompletion();
     }
 
     private void Awake()
