1c567fd0797377b3db31cdf934f705c7f940c30b
CodeyHuntting <creativecodey@gmail.com>
2026-05-14 10:16:18 -0500

    added an end turn button

---
+++ Assets/Scripts/Player/EndTurnButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class EndTurnButton : MonoBehaviour
+{
+    private Button _button;
+    private Battle _battle;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _battle = GetComponentInParent<Battle>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked()
+    {
+        _battle.StartNextTurn();
+    }
+}
