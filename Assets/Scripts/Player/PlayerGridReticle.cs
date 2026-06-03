d33d905b44a485d58103ba6658c41561d4355d45
CodeyHuntting <creativecodey@gmail.com>
2026-06-03 10:07:51 -0500

    breath looks pretty good

--- Assets/Scripts/Player/PlayerGridReticle.cs
+++ Assets/Scripts/Player/PlayerGridReticle.cs
@@ -6,9 +6,23 @@ public class PlayerGridReticle : MonoBehaviour
     [SerializeField] private float _moveDuration = .2f;
     [SerializeField] private Ease _moveEase = Ease.OutQuad;
 
+    [Space]
+    [SerializeField] private float _breathDuration = 1f;
+    [SerializeField] private Vector2 _breathScaleRange = new(.8f, 1f);
+
+    private Tween _moveTween;
+
+    private void Awake()
+    {
+        transform.localScale = Vector2.one * _breathScaleRange.x;
+        transform.DOScale(_breathScaleRange.y, _breathDuration * .5f)
+            .SetEase(Ease.OutQuad)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
     public void MoveToPosition(Vector2 position)
     {
-        transform.DOKill();
-        transform.DOMove(position, _moveDuration).SetEase(_moveEase);
+        _moveTween?.Kill();
+        _moveTween = transform.DOMove(position, _moveDuration).SetEase(_moveEase);
     }
 }
