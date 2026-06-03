acb496a7be4ae24239dba5db417fcbb4625b4f9f
CodeyHuntting <creativecodey@gmail.com>
2026-06-03 10:18:37 -0500

    made the animation look more like a breath

--- Assets/Scripts/Player/PlayerGridReticle.cs
+++ Assets/Scripts/Player/PlayerGridReticle.cs
@@ -7,17 +7,24 @@ public class PlayerGridReticle : MonoBehaviour
     [SerializeField] private Ease _moveEase = Ease.OutQuad;
 
     [Space]
-    [SerializeField] private float _breathDuration = 1f;
-    [SerializeField] private Vector2 _breathScaleRange = new(.8f, 1f);
+    [SerializeField] private float _breathInScale = 1;
+    [SerializeField] private float _breathInDuration = .5f;
+    [SerializeField] private Ease _breathInEase = Ease.OutQuad;
+    [SerializeField] private float _breathOutScale = .8f;
+    [SerializeField] private float _breathOutDuration = 1f;
+    [SerializeField] private Ease _breatOutEase = Ease.Linear;
 
     private Tween _moveTween;
 
     private void Awake()
     {
-        transform.localScale = Vector2.one * _breathScaleRange.x;
-        transform.DOScale(_breathScaleRange.y, _breathDuration * .5f)
-            .SetEase(Ease.OutQuad)
-            .SetLoops(-1, LoopType.Yoyo);
+        transform.localScale = Vector2.one * _breathOutScale;
+        Tween breathInTween = transform.DOScale(_breathInScale, _breathInDuration).SetEase(_breathInEase);
+        Tween breathOutTween = transform.DOScale(_breathOutScale, _breathOutDuration).SetEase(_breatOutEase);
+        DOTween.Sequence()
+            .Append(breathInTween)
+            .Append(breathOutTween)
+            .SetLoops(-1, LoopType.Restart);
     }
 
     public void MoveToPosition(Vector2 position)
