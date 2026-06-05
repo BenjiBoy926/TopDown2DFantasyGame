f117f930a75cbd4fce58a40fd73f446752d67fea
CodeyHuntting <creativecodey@gmail.com>
2026-06-05 11:10:57 -0500

    no text color change

--- Assets/Scripts/Character/UI/CharacterHealthText.cs
+++ Assets/Scripts/Character/UI/CharacterHealthText.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 [RequireComponent(typeof(TMP_Text))]
 public class CharacterHealthText : MonoBehaviour
 {
-    private CharacterStats _stats;
     private TMP_Text _label;
+
+    private void Awake()
+    {
+        _label = GetComponent<TMP_Text>();
+    }
 
     public void ShowHealth(int health)
     {
         _label.text = health.ToString();
-        _label.color = _stats.GetHealthColor(health);
-    }
-
-    private void Awake()
-    {
-        _stats = GetComponentInParent<CharacterStats>();
-        _label = GetComponent<TMP_Text>();
     }
 }
