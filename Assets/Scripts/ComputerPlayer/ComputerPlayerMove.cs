d92d2877c3352959217230fb18d7bf18d2e96aa9
CodeyHuntting <creativecodey@gmail.com>
2026-06-19 15:04:47 -0500

    they do move but in ways that are horrifying XD

--- Assets/Scripts/ComputerPlayer/ComputerPlayerMove.cs
+++ Assets/Scripts/ComputerPlayer/ComputerPlayerMove.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
@@ -16,60 +17,64 @@ public class ComputerPlayerMove : MonoBehaviour
         _computerPlayer = GetComponent<ComputerPlayer>();
     }
 
-    public Coroutine Move(Character character)
+    public IEnumerator Move(Character character)
     {
         _character = character;
         return Move();
     }
 
-    private Coroutine Move()
+    private IEnumerator Move()
     {
         _character.RefreshRange();
-        GetAttackableCharacters(_character, _attackableCharacters);
+        GetAttackableCharacters(_attackableCharacters);
         if (_attackableCharacters.Count > 0)
         {
-            return AttackSomeone(_character, _attackableCharacters);
+            yield return AttackSomeone(_attackableCharacters);
         }
         else
         {
             // TODO: move to a better position first
-            return _character.Defend();
+            yield return _character.Defend();
         }
     }
 
-    private void GetAttackableCharacters(Character character, List<Character> attackable)
+    private void GetAttackableCharacters(List<Character> attackable)
     {
         attackable.Clear();
         foreach (var other in _computerPlayer.AllCharacters)
         {
-            if (IsAttackable(character, other))
+            if (IsAttackable(other))
             {
                 attackable.Add(other);
             }
         }
     }
 
-    private bool IsAttackable(Character character, Character target)
+    private bool IsAttackable(Character target)
     {
-        return character != target &&
-            character.Faction != target.Faction &&
-            character.IsReachable(target.CurrentCell) &&
+        return _character != target &&
+            _character.Faction != target.Faction &&
+            _character.IsReachable(target.CurrentCell) &&
             !target.IsDead;
     }
 
-    private Coroutine AttackSomeone(Character character, List<Character> attackable)
+    private IEnumerator AttackSomeone(List<Character> attackable)
     {
-        Character target = GetBestTarget(character, attackable);
-        return character.Attack(target);
+        Character target = GetBestTarget(attackable);
+        Vector2Int adjacentCell = GetBestAdjacentTile(target);
+        Vector2 adjacentPosition = _character.CellToWorld(adjacentCell);
+        _character.SetIsRunning(true);
+        yield return _character.transform.DOMove(adjacentPosition, 1).WaitForCompletion();
+        yield return _character.Attack(target);
     }
 
-    private Character GetBestTarget(Character character, List<Character> attackable)
+    private Character GetBestTarget(List<Character> attackable)
     {
         Character bestTarget = null;
         float bestScore = float.MinValue;
         foreach (var target in attackable)
         {
-            float score = GetAttackScore(character, target);
+            float score = GetAttackScore(target);
             if (score > bestScore)
             {
                 bestScore = score;
@@ -79,36 +84,36 @@ public class ComputerPlayerMove : MonoBehaviour
         return bestTarget;
     }
 
-    private float GetAttackScore(Character character, Character target)
+    private float GetAttackScore(Character target)
     {
-        return GetAttackHealthScore(character, target) + GetAttackTravelScore(character, target);
+        return GetAttackHealthScore(target) + GetAttackTravelScore(target);
     }
 
-    private static float GetAttackTravelScore(Character character, Character target)
+    private float GetAttackTravelScore(Character target)
     {
         Vector2Int targetCell = target.CurrentCell;
-        return GetTravelScore(character, targetCell);
+        return GetTravelScore(targetCell);
     }
 
-    private static float GetAttackHealthScore(Character character, Character target)
+    private float GetAttackHealthScore(Character target)
     {
         int healthBeforeAttack = target.CurrentHealth;
-        int healthAfterAttack = target.CalculateHealthAfterHitFrom(character);
+        int healthAfterAttack = target.CalculateHealthAfterHitFrom(_character);
         int damageDealt = healthBeforeAttack - healthAfterAttack;
         return (float)damageDealt / healthBeforeAttack;
     }
 
-    private Vector2Int GetBestAdjacentTile(Character character, Character target)
+    private Vector2Int GetBestAdjacentTile(Character target)
     {
         CellNeighbors neighbors = CellNeighbors.Get(target.CurrentCell);
         List<Vector2Int> adjacentCells = new() { neighbors.Left, neighbors.Right, neighbors.Up, neighbors.Down };
         return adjacentCells[0]; // todo pick best stayable cell
     }
 
-    private static float GetTravelScore(Character character, Vector2Int targetCell)
+    private float GetTravelScore(Vector2Int targetCell)
     {
-        Vector2Int myCell = character.CurrentCell;
+        Vector2Int myCell = _character.CurrentCell;
         int rectDistance = CharacterRange.RectangularDistance(myCell, targetCell);
-        return (float)rectDistance / character.TraversalRange;
+        return (float)rectDistance / _character.TraversalRange;
     }
 }
