64c45b802979b34bff8219405d901117fc246591
CodeyHuntting <creativecodey@gmail.com>
2026-07-08 16:28:32 -0500

    drawing up more of the algorithm but not finished yet

--- Assets/Scripts/ComputerPlayer/ComputerPlayerPathfinder.cs
+++ Assets/Scripts/ComputerPlayer/ComputerPlayerPathfinder.cs
@@ -1,20 +1,119 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.Tilemaps;
 
 public class ComputerPlayerPathfinder : MonoBehaviour
 {
     public class Node
     {
+        public int Cost => Parent != null ? Parent.Cost + 1 : 0;
+
         public Vector2Int Cell;
         public Node Parent;
     }
 
-    private List<Vector2Int> _path = new();
+    public class NodeNeighbors
+    {
+        public Node Left, Right, Up, Down;
+
+        public static NodeNeighbors Get(Node node)
+        {
+            CellNeighbors neighbors = CellNeighbors.Get(node.Cell);
+            return new NodeNeighbors
+            {
+                Left = new Node { Cell = neighbors.Left, Parent = node },
+                Right = new Node { Cell = neighbors.Right, Parent = node },
+                Up = new Node { Cell = neighbors.Up, Parent = node },
+                Down = new Node { Cell = neighbors.Down, Parent = node }
+            };
+        }
+    }
+
+    private Character _character;
+    private Vector2Int _target;
+
+    private static readonly HashSet<Node> _visited = new();
+    private static readonly List<Node> _next = new();
 
     public List<Vector2Int> FindPath(Character character, Vector2Int target)
     {
-        _path.Clear();
-        // add them
-        return _path;
+        _character = character;
+        _target = target;
+
+        _visited.Clear();
+        _next.Clear();
+
+        Node start = new() { Cell = character.CurrentCell, Parent = null };
+        Enqueue(start);
+
+        while (_next.Count > 0)
+        {
+            Node current = Dequeue();
+            if (current.Cell == target)
+            {
+                // TODO return actual path
+                return new();
+            }
+
+
+        }
+
+        return new();
+    }
+
+    private void VisitNeighbors(Node node)
+    {
+        NodeNeighbors neighbors = NodeNeighbors.Get(node);
+
+    }
+
+    private void Visit(Node node)
+    {
+        
+    }
+
+    private bool ShouldEnqueue(Node node)
+    {
+        return !_visited.Contains(node) && IsTraversible(node);
+    }
+
+    private bool IsTraversible(Node node)
+    {
+        // TODO: get this through the _character field
+        return true;
+        //if (node.Cost > _character.TraversalRange)
+        //{
+        //    return false;
+        //}
+
+        //TileBase tile = _character.GetTile(node.Cell);
+        //if (tile && _wallTiles.Contains(tile))
+        //{
+        //    return false;
+        //}
+
+        //Character occupant = _character.GetOccupant(node.Cell);
+        //bool canMoveThroughOccupant = !occupant || occupant.Faction == _character.Faction;
+        //return canMoveThroughOccupant;
+    }
+
+    private void Enqueue(Node node)
+    {
+        if (_visited.Contains(node))
+            return;
+
+        _visited.Add(node);
+        _next.Add(node);
+    }
+
+    private Node Dequeue()
+    {
+        if (_next.Count == 0)
+            return null;
+
+        Node node = _next[0];
+        _next.RemoveAt(0);
+        return node;
     }
 }
