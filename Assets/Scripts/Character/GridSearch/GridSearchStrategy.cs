1d20159152d8237f908dcfc0b808db697ca2a72b
CodeyHuntting <creativecodey@gmail.com>
2026-07-15 10:43:27 -0500

    added a property for later

--- Assets/Scripts/Character/GridSearch/GridSearchStrategy.cs
+++ Assets/Scripts/Character/GridSearch/GridSearchStrategy.cs
@@ -1,5 +1,11 @@
+using System;
+
 public abstract class GridSearchStrategy
 {
+    public Comparison<Node> NodeComparison => _nodeComparison ??= CompareNodes;
+
+    private Comparison<Node> _nodeComparison;
+
     public abstract bool ShouldEnqueue(Node node);
     public abstract int CompareNodes(Node a, Node b);
     public abstract bool IsExitNode(Node node);
