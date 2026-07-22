12fa470d6b6a07cf251db4fb7ca452326225e415
CodeyHuntting <creativecodey@gmail.com>
2026-07-22 15:21:05 -0500

    success

--- Assets/Scripts/BattleCameraFollow.cs
+++ Assets/Scripts/BattleCameraFollow.cs
@@ -1,39 +1,66 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
-
 
 public class BattleCameraFollow : MonoBehaviour
 {
     [SerializeField] private float _initialFollowDuration = 0.35f;
-    [SerializeField] private Ease _initialFollowEase = Ease.OutQuad;
+    [SerializeField] private AnimationCurve _initialFollowEase;
     private Transform _target;
-    private Tween _initialFollowTween;
+    private Coroutine _initialFollowRoutine;
 
     public void Begin(Transform target)
     {
-        Vector3 targetPosition = GetCameraPositionFromFollowTarget(target);
         _target = target;
-        _initialFollowTween = transform.DOMove(targetPosition, _initialFollowDuration).SetEase(_initialFollowEase);
+        StopAllCoroutines();
+        _initialFollowRoutine = StartCoroutine(GetInitialFollowRoutine());
     }
 
     private void Update()
     {
-        if (_initialFollowTween.IsActive() || !_target)
-        {
+        if (!_target)
             return;
+
+        if (_target && _initialFollowRoutine == null)
+        {
+            transform.position = GetTargetPosition();
         }
-        transform.position = GetCameraPositionFromFollowTarget(_target);
     }
 
     public void End()
     {
         _target = null;
-        _initialFollowTween.Kill();
+        StopAllCoroutines();
+        _initialFollowRoutine = null;
     }
 
-    private Vector3 GetCameraPositionFromFollowTarget(Transform target)
+    private IEnumerator GetInitialFollowRoutine()
     {
-        Vector2 targetPosition = target.position;
+        Vector3 startPosition = transform.position;
+
+        float startTime = Time.time;
+        float elapsedTime = Time.time - startTime;
+
+        while (elapsedTime < _initialFollowDuration)
+        {
+            float progress = elapsedTime / _initialFollowDuration;
+            Vector3 endPosition = GetTargetPosition();
+            float t = _initialFollowEase.Evaluate(progress);
+            Vector3 currentPosition = Vector3.Lerp(startPosition, endPosition, t);
+            transform.position = currentPosition;
+
+            yield return null;
+
+            elapsedTime = Time.time - startTime;
+        }
+
+        transform.position = GetTargetPosition();
+        _initialFollowRoutine = null;
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        Vector2 targetPosition = _target.position;
         float z = transform.position.z;
         return new(targetPosition.x, targetPosition.y, z);
     }
