242c375b8439fa68c42e00b894c5521890223b1b
CodeyHuntting <creativecodey@gmail.com>
2026-07-29 14:48:45 -0500

    different method that will make masking easier

--- Assets/Scripts/Player/RangeWarning.cs
+++ Assets/Scripts/Player/RangeWarning.cs
@@ -27,10 +27,13 @@ public class RangeWarning : MonoBehaviour
 
     private void Update()
     {
-        Vector3 attackerPosition = _attacker.Position;
-        Vector3 targetPosition = _target.Position;
+        Vector2 attackerPosition = _attacker.Position;
+        Vector2 targetPosition = _target.Position;
+        float distance = Vector2.Distance(attackerPosition, targetPosition);
+
         transform.position = (attackerPosition + targetPosition) / 2;
         transform.up = targetPosition - attackerPosition;
+        transform.localScale = new(1, distance, 1);
     }
 
     private void SetIsVisible(bool isVisible)
