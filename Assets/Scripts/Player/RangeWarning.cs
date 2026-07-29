5c963a37b374f1718beef8e05a74ccdfe1a79700
CodeyHuntting <creativecodey@gmail.com>
2026-07-29 14:30:44 -0500

    working on an arrow

--- Assets/Scripts/Player/RangeWarning.cs
+++ Assets/Scripts/Player/RangeWarning.cs
@@ -1,30 +1,16 @@
-using DG.Tweening;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 public class RangeWarning : MonoBehaviour
 {
     [SerializeField] private float _fadeDuration = 0.2f;
     private Character _attacker;
     private Character _target;
-    private LineRenderer _line;
-    private readonly Vector3[] _positions = new Vector3[]
-    {
-        Vector3.zero, Vector3.zero
-    };
     private bool _isVisible = false;
-
-    private void Awake()
-    {
-        _line = GetComponent<LineRenderer>();
-    }
 
     public void Begin(Character attacker, Character target)
     {
         _attacker = attacker;
         _target = target;
-        _positions[0] = _attacker.transform.position;
         Refresh();
     }
 
@@ -41,8 +27,10 @@ public class RangeWarning : MonoBehaviour
 
     private void Update()
     {
-        _positions[1] = _target.transform.position;
-        _line.SetPositions(_positions);
+        Vector3 attackerPosition = _attacker.Position;
+        Vector3 targetPosition = _target.Position;
+        transform.position = (attackerPosition + targetPosition) / 2;
+        transform.up = targetPosition - attackerPosition;
     }
 
     private void SetIsVisible(bool isVisible)
@@ -63,19 +51,11 @@ public class RangeWarning : MonoBehaviour
 
     private void FadeIn()
     {
-        FadeAlpha(1);
+        gameObject.SetActive(true);
     }
 
     private void FadeOut()
     {
-        FadeAlpha(0);
-    }
-
-    private void FadeAlpha(float alpha)
-    {
-        Color color = Color.red;
-        color.a = alpha;
-        Color2 value = new(color, color);
-        _line.DOColor(value, value, _fadeDuration);
+        gameObject.SetActive(false);
     }
 }
