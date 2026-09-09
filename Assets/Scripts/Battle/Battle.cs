42faba92220f2f6c5f6f12e6820cdd86ddb200cc
CodeyHuntting <creativecodey@gmail.com>
2026-09-09 10:42:13 -0500

    unused method

--- Assets/Scripts/Battle/Battle.cs
+++ Assets/Scripts/Battle/Battle.cs
@@ -86,11 +86,6 @@ public class Battle : MonoBehaviour
         _turn.SetCurrentTurn(faction);
     }
 
-    public void GetCharactersInFaction(Faction faction, List<Character> result)
-    {
-        _turn.GetCharactersInFaction(faction, result);
-    }
-
     public int CountMoveableCharacters(Faction faction)
     {
         return _turn.CountMoveableCharacters(faction);
