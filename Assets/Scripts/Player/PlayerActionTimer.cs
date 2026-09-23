0dc51a6a814e2c633330f6299d3bb6d742e11847
CodeyHuntting <creativecodey@gmail.com>
2026-09-23 10:10:45 -0500

    end turn has a separate longer duration

--- Assets/Scripts/Player/PlayerActionTimer.cs
+++ Assets/Scripts/Player/PlayerActionTimer.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class PlayerActionTimer : MonoBehaviour
 {
-    [SerializeField] private float _duration = 0.5f;
+    [SerializeField] private float _undoRedoDuration = 0.5f;
+    [SerializeField] private float _endTurnDuration = 1.5f;
     private Battle _battle;
     private PlayerActionTimerUI _ui;
     private Action _undoAction;
@@ -25,8 +26,8 @@ public class PlayerActionTimer : MonoBehaviour
         if (!_battle.IsUndoAvailable())
             return;
 
-        Begin(_undoAction);
-        _ui.BeginUndo(_duration);
+        Begin(_undoAction, _undoRedoDuration);
+        _ui.BeginUndo(_undoRedoDuration);
     }
 
     public void BeginRedo()
@@ -34,15 +35,14 @@ public class PlayerActionTimer : MonoBehaviour
         if (!_battle.IsRedoAvailable())
             return;
 
-        Begin(_redoAction);
-        _ui.BeginRedo(_duration);
+        Begin(_redoAction, _undoRedoDuration);
+        _ui.BeginRedo(_undoRedoDuration);
     }
 
     public void BeginEndTurn()
     {
-        Begin(_endTurnAction);
-        // TODO: longer duration for end turn
-        _ui.BeginEndTurn(_duration);
+        Begin(_endTurnAction, _endTurnDuration);
+        _ui.BeginEndTurn(_endTurnDuration);
     }
 
     public void CancelUndo()
@@ -60,10 +60,10 @@ public class PlayerActionTimer : MonoBehaviour
         Cancel(_endTurnAction);
     }
 
-    private void Begin(Action action)
+    private void Begin(Action action, float duration)
     {
         _pendingAction = action;
-        Invoke(nameof(Confirm), _duration);
+        Invoke(nameof(Confirm), duration);
     }
 
     private void Cancel(Action action)
